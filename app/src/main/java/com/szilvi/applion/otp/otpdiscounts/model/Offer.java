5e34d1836e5dc515746efbf2fc9441a5792a87d7
Phaedra144 <bedi.szilvia@gmail.com>
2019-01-23 10:41:32 +0100

    feat(model-offer-response)created

--- app/src/main/java/com/szilvi/applion/otp/otpdiscounts/model/Offer.java
+++ app/src/main/java/com/szilvi/applion/otp/otpdiscounts/model/Offer.java
@@ -1,8 +1,19 @@
 package com.szilvi.applion.otp.otpdiscounts.model;
 
+import com.szilvi.applion.otp.otpdiscounts.response.OfferResponse;
+
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Offer {
 
     int id;
