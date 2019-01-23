918e73cba290f8407d85eb3b4bfc4a0e44ee9505
Phaedra144 <bedi.szilvia@gmail.com>
2019-01-23 10:21:56 +0100

    init(otp-discount-project)

---
+++ app/src/main/java/com/szilvi/applion/otp/otpdiscounts/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.szilvi.applion.otp.otpdiscounts.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.szilvi.applion.otp.otpdiscounts.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
