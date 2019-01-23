29d520aadb2cbe46af37ad8a74f8422359fe1845
Phaedra144 <bedi.szilvia@gmail.com>
2019-01-23 10:26:27 +0100

    feat(offer) model created, not final

--- app/src/main/java/com/szilvi/applion/otp/otpdiscounts/activity/MainActivity.java
+++ app/src/main/java/com/szilvi/applion/otp/otpdiscounts/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.szilvi.applion.otp.otpdiscounts.activities;
+package com.szilvi.applion.otp.otpdiscounts.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
---
+++ app/src/main/java/com/szilvi/applion/otp/otpdiscounts/model/Offer.java
@@ -0,0 +1,15 @@
+package com.szilvi.applion.otp.otpdiscounts.model;
+
+import java.time.LocalDateTime;
+
+public class Offer {
+
+    int id;
+    LocalDateTime endTime;
+    String title;
+    String subTitle;
+    String partnerName;
+    String description;
+    String state;
+
+}
